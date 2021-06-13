e56cb0ed8e89477da080fe30b91bcdfb674d7685
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-06-13 12:42:23 -0400

    converted to jsx

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+import { render } from 'react-dom';
 import Pet from './Pet';
 
-const App = () => {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, 'Adopt Me!'),
-		React.createElement(Pet, {
-			name: 'Luna',
-			animal: 'Dog',
-			breed: 'Havanese',
-		}),
-		React.createElement(Pet, {
-			name: 'Pepper',
-			animal: 'Bird',
-			breed: 'Cockatiel',
-		}),
-		React.createElement(Pet, {
-			name: 'Doink',
-			animal: 'Cat',
-			breed: 'Mix',
-		}),
-	]);
-};
+// const App = () => {
+// 	return React.createElement('div', {}, [
+// 		React.createElement('h1', {}, 'Adopt Me!'),
+// 		React.createElement(Pet, {
+// 			name: 'Luna',
+// 			animal: 'Dog',
+// 			breed: 'Havanese',
+// 		}),
+// 		React.createElement(Pet, {
+// 			name: 'Pepper',
+// 			animal: 'Bird',
+// 			breed: 'Cockatiel',
+// 		}),
+// 		React.createElement(Pet, {
+// 			name: 'Doink',
+// 			animal: 'Cat',
+// 			breed: 'Mix',
+// 		}),
+// 	]);
+// };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+//  ** JSX translates the code below into the React.createElement calls above **
+
+export default function App() {
+	return (
+		<div>
+			<h1>Adopt Me!</h1>
+			<Pet name="Luna" animal="dog" breed="Havanese" />
+			<Pet name="Pepper" animal="bird" breed="Cockatiel" />
+			<Pet name="Doink" animal="Cat" breed="Mix" />
+		</div>
+	);
+}
+
+// ReactDOM.render(React.createElement(App), document.getElementById('root')); <--- old way with createElement
+
+render(<App />, document.getElementById('root'));
--- src/Pet.js
+++ src/Pet.js
@@ -1,9 +1,33 @@
-import React from 'react';
+// import React from 'react';
+
+// export default function Pet({ name, animal, breed }) {
+// 	return React.createElement('div', {}, [
+// 		React.createElement('h1', {}, name),
+// 		React.createElement('h2', {}, animal),
+// 		React.createElement('h2', {}, breed),
+// 	]);
+// }
+
+// ** same as above but with JSX **
+
+// export default function Pet(props) {
+// 	return (
+// 		<div>
+// 			<h2>{props.name}</h2>
+// 			<h3>{props.animal}</h3>
+// 			<h3>{props.breed}</h3>
+// 		</div>
+// 	);
+// }
+
+// ** same as above but with destructuring **
 
 export default function Pet({ name, animal, breed }) {
-	return React.createElement('div', {}, [
-		React.createElement('h1', {}, name),
-		React.createElement('h2', {}, animal),
-		React.createElement('h2', {}, breed),
-	]);
-}
+	return (
+		<div>
+			<h2>{name}</h2>
+			<h3>{animal}</h3>
+			<h3>{breed}</h3>
+		</div>
+	);
+};
