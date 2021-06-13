2fe08de5210fd046b7783d539a60f649363c1468
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-06-12 20:39:09 -0400

    added babel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Pet from "./Pet";
+import Pet from './Pet';
 
 const App = () => {
 	return React.createElement('div', {}, [
@@ -15,7 +15,11 @@ const App = () => {
 			animal: 'Bird',
 			breed: 'Cockatiel',
 		}),
-		React.createElement(Pet, { name: 'Doink', animal: 'Cat', breed: 'Mix' }),
+		React.createElement(Pet, {
+			name: 'Doink',
+			animal: 'Cat',
+			breed: 'Mix',
+		}),
 	]);
 };
 
