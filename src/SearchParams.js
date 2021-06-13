dff364b72d334a983fd7a92c8c06a6366a2e48aa
Stephan Lamoureux <sa.lamoureux@gmail.com>
2021-06-13 14:02:05 -0400

    added comments

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -4,6 +4,8 @@ const SearchParams = () => {
 	// const locationTuple = useState("Boston, MA");
 	// const location = locationTuple[0];
 	// const setLocation = locationTuple[1];
+
+	// ****** do not put hooks inside if statements or loops! ******
 	const [location, setLocation] = useState('Boston, MA'); // <--- same as above, but w/ destructuring
 
 	return (
@@ -23,5 +25,11 @@ const SearchParams = () => {
 		</div>
 	);
 };
+
+/*
+We use the setLocation function in the onChange attribute of the input.
+Every time the input is typed into, it's going to call that function which calls setLocation with what has been typed into the input.
+When setLocation is called, React knows that its state has been modified and kicks off a re-render.
+*/
 
 export default SearchParams;
